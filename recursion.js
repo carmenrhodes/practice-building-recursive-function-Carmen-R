389860db99105f9bbe14e8ee00241cffa920b2d7
CarmenRhodes <carmenrhodes289@gmail.com>
2025-04-22 21:17:58 -0500

    Completed Lesson 4 Practice Building a Recursive Function

--- recursion.js
+++ recursion.js
@@ -1,8 +1,8 @@
 function reverseString(string) {
-    if (string.length === 1);
+    if (string.length <= 1) {
         return string;
-};
-return reverseString(string.length -1);
+    }
+    return string.charAt(string.length - 1) + reverseString(string.slice(0, -1));
+}
 
-console.log(reverseString("Hello"));
-
+console.log(reverseString("dow"));
